7a8b19f44dc10aa46efbb4feee32f71e78300786
hectorCBP <36700896+hectorCBP@users.noreply.github.com>
2018-07-22 22:34:51 -0300

    Remueve formAdmin
    Se remueve formAdmin, no tenia sentido
    en nuestro sitio.
    A demas se cambia la redireccion del
    login para admin y pasa a formHab por defecto

--- Presentacion/Default.aspx.cs
+++ Presentacion/Default.aspx.cs
@@ -36,7 +36,7 @@ public partial class _Default : System.Web.UI.Page
                 }
                 else
                 {
-                    Response.Redirect("formAdmin.aspx");
+                    Response.Redirect("formHab.aspx");
                 }
             }
         }
